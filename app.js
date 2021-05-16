eb56242cce2616c6cde6dc260748a21019c5c4c4
jainish <jainishkunjadiya2712@gmail.com>
2021-05-16 10:48:40 +0530

    nevabar id changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.set("views", path.join(__dirname, "views"));
 //ENDPOINTS
 app.get("/", (req, res) => {
   const params = {};
-  res.status(200).render("index.pug", params);
+  res.status(200).render("myActivity.pug", params);
 });
 
 //start server
