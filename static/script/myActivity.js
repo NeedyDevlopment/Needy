59ae53dd6f632853204758ce81ec45fbc68d0cf9
jainish <jainishkunjadiya2712@gmail.com>
2021-05-16 18:07:08 +0530

    myactivity front work

--- static/script/myActivity.js
+++ static/script/myActivity.js
@@ -1,5 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
   var element = document.getElementById("myActivity");
   element.classList.add("active");
-  console.log(element);
+  var activity = document.getElementById("activity");
+  activity.style = "display:none;";
 });
+function post() {
+  var color = document.getElementById("colourful");
+  var postbutton = document.getElementById("postbtn");
+  var activitybutton = document.getElementById("activitybtn");
+  var activity = document.getElementById("activity");
+  var post = document.getElementById("post");
+  color.style = "left:0;";
+  postbutton.style = "color:white;";
+  activitybutton.style = "color:black;";
+  activity.style = "display:none;";
+  post.style = "display:block;";
+}
+function activity() {
+  var color = document.getElementById("colourful");
+  var postbutton = document.getElementById("postbtn");
+  var activitybutton = document.getElementById("activitybtn");
+  var activity = document.getElementById("activity");
+  var post = document.getElementById("post");
+  color.style = "left:50%;";
+  postbutton.style = "color:black;";
+  activitybutton.style = "color:white;";
+  activity.style = "display:block;";
+  post.style = "display:none;";
+}
