4756082c75db52201a04207923c9c69ac15d24ea
Raj Jain <jainraj23112000@gmail.com>
2021-05-20 13:10:49 +0530

    modal done

--- static/script/signup.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var a = document.getElementById("Signupbtn");
-    a.classList.add("active");
-    // this.last.backgroundColor = "red";
-});
