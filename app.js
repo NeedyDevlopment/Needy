52174cf31b76e16be3e8c2c9079ac348180db51c
Raj Jain <jainraj23112000@gmail.com>
2021-06-04 10:49:15 +0530

    login signup contact us done

--- app.js
+++ app.js
@@ -17,10 +17,7 @@ app.get("/", (req, res) => {
   const params = { likes: 10, comments: 20 };
   res.status(200).render("myActivity.pug", params);
 });
-app.get("/home", (req, res) => {
-  const params = { };
-  res.status(200).render("home.pug", params);
-});
+
 
 app.get("/signup", (req, res) => {
   const params = { };
--- static/script/signup.js
+++ static/script/signup.js
@@ -1,3 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var element = document.getElementById("Singupbtn");
+  element.classList.add("active");
+  // var activity = document.getElementById("activity");
+  // activity.style = "display:none;";
+});
 function InvalidMsg(textbox) {
   if (textbox.value === "") {
     textbox.setCustomValidity("Entering a username is necessary!");
@@ -6,35 +12,29 @@ function InvalidMsg(textbox) {
   }
 }
 function InvalidEmail(textbox) {
+  var x = document.form.email.value;
+  var atposition = x.indexOf("@");
+  var dotposition = x.lastIndexOf(".");
   if (textbox.value === "") {
     textbox.setCustomValidity("Entering an email is necessary!");
-  } else if (textbox.validity.typeMismatch) {
+  } else if (
+    atposition < 1 ||
+    dotposition < atposition + 2 ||
+    dotposition + 2 >= x.length
+  ) {
     textbox.setCustomValidity("Please enter an email address which is valid!");
   } else {
     textbox.setCustomValidity("");
   }
 }
+
 function Invalidpass(textbox) {
-  var lowerCaseLetters = "/[a-z]/g";
-  var upperCaseLetters = "/[A-Z]/g";
-  var numbers = "/[0-9]/g";
+  var pass = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{7,15}$/;
   if (textbox.value === "") {
     textbox.setCustomValidity("Entering a password is necessary!");
-  } else if (textbox.value.match(lowerCaseLetters)) {
+  } else if (!textbox.value.match(pass)) {
     textbox.setCustomValidity(
-      "Must contain at least one number and one uppercase and lowercase letter and  and at least 8 or more characters"
-    );
-  } else if (textbox.value.match(upperCaseLetters)) {
-    textbox.setCustomValidity(
-      "Must contain at least one number and one uppercase and lowercase letter and  and at least 8 or more characters"
-    );
-  } else if (textbox.value.match(numbers)) {
-    textbox.setCustomValidity(
-      "Must contain at least one number and one uppercase and lowercase letter and  and at least 8 or more characters"
-    );
-  } else if (textbox.value.legnth >= 8) {
-    textbox.setCustomValidity(
-      "Must contain at least one number and one uppercase and lowercase letter and  and at least 8 or more characters"
+      "Must contain at least one number and uppercase and lowercase letter and one special, and at least 7 or more characters"
     );
   } else {
     textbox.setCustomValidity("");
@@ -49,3 +49,38 @@ function myFunction() {
     x.type = "password";
   }
 }
+
+function Invalidcpass(textbox) {
+  var cpass = document.getElementById("pass").value;
+  if (textbox.value === "") {
+    textbox.setCustomValidity("Entering a confirm password is necessary!");
+  } else if (!textbox.value.match(cpass)) {
+    textbox.setCustomValidity("Must contain password is match!");
+  } else {
+    textbox.setCustomValidity("");
+  }
+}
+function Invalidcity(textbox) {
+  if (textbox.value === "") {
+    textbox.setCustomValidity("Entering a city is necessary!");
+  } else {
+    textbox.setCustomValidity("");
+  }
+}
+
+function Invalidnumber(textbox) {
+  if (textbox.value === "") {
+    textbox.setCustomValidity("Entering a contact number is necessary!");
+  } else {
+    textbox.setCustomValidity("");
+  }
+}
+function Invalidplace(textbox) {
+  if (textbox.value === "") {
+    textbox.setCustomValidity("Entering a work place is necessary!");
+  } else {
+    textbox.setCustomValidity("");
+  }
+}
+
+
