a7fc25237bde875932f814d008b4acbe0c5618ea
jainish <jainishkunjadiya2712@gmail.com>
2021-06-11 09:58:22 +0530

    profile page

--- static/script/profile.js
+++ static/script/profile.js
@@ -4,6 +4,7 @@ $(document).ready(function () {
   $("input").attr("disabled", true);
   $("#saveBtn").hide();
   $("#cancelBtn").hide();
+  $(".cameraIcon").css("opacity", 0);
   $("input").click(function () {
     $("input").removeClass("active");
     $(this).addClass("active");
@@ -12,6 +13,7 @@ $(document).ready(function () {
   $("#editBtn").click(function () {
     $("input").attr("disabled", false);
     $("#email").attr("disabled", true);
+    $(".cameraIcon").css("opacity", 0.5);
     $("#saveBtn").show(400);
     $("#cancelBtn").show(400);
     $("#editBtn").hide(400);
@@ -22,6 +24,7 @@ $(document).ready(function () {
     $("input").attr("disabled", true);
     $("#saveBtn").hide(400);
     $("#cancelBtn").hide(400);
+    $(".cameraIcon").css("opacity", 0);
     $("#editBtn").show(400);
     $("#changePass").show(400);
     $("#deleteBtn").show(400);
