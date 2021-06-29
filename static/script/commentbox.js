26c582622357446f944458e97bac907e99ae64e2
jainish <jainishkunjadiya2712@gmail.com>
2021-06-29 15:24:29 +0530

    comment box

--- static/script/commentbox.js
+++ static/script/commentbox.js
@@ -1,20 +1,17 @@
-function showcommentbox() {
-    console.log('showcommentbox called!');
-    var commentbox = document.getElementById('maincommentcontainer');
-    commentbox.style.animation = 'fadeIn 1s';
-    commentbox.classList.add('show-commentbox');
+function showcommentbox(postId) {
+  $(".maincommentcontainer").removeClass("show-commentbox");
+  console.log("showcommentbox called!");
+  var commentbox = document.getElementById("maincommentcontainer" + postId);
+  commentbox.style.animation = "fadeIn 1s";
+  commentbox.classList.add("show-commentbox");
 }
 
-function hidecommentbox(animate) {
-    console.log('hidecommentbox called!');
-    var commentbox = document.getElementById('maincommentcontainer');
-    // if (animate) {
-    commentbox.style.animation = 'fadeOut 1s';
-    setTimeout(() => {
-        commentbox.classList.remove('show-commentbox');
-    }, 1000);
-    // } else {
-    //     commentbox.classList.remove('show-commentbox');
-    // }
-    // commentbox.classList.remove('show-commentbox');
+function hidecommentbox(postId) {
+  console.log("hidecommentbox called!");
+  var commentbox = document.getElementById("maincommentcontainer" + postId);
+  // if (animate) {
+  commentbox.style.animation = "fadeOut 1s";
+  setTimeout(() => {
+    commentbox.classList.remove("show-commentbox");
+  }, 1000);
 }
