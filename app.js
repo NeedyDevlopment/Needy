868a14c600757daa097ef49fcbf1fad274bc1621
Jainish Kunjadiya <jainishkunjadiya2712@gmail.com>
2021-09-30 16:26:07 +0530

    F list clicked solved

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.use((req, res, next) => {
-    res.setHeader("Service-Worker-Allowed", "/");
-    next();
+  res.setHeader("Service-Worker-Allowed", "/");
+  next();
 });
 
 //router imports
@@ -81,13 +81,10 @@ app.use("/", homePageRouter);
 app.use("/showModal", showModalRouter);
 app.use("/otpOperation", otpOperationRouter);
 app.use("/pwOperation", pwOperationRouter);
-app.use(
-    "/getContentOnScrollForMyActivity",
-    getContentOnScrollForMyActivityRouter
-);
+app.use("/getContentOnScrollForMyActivity", getContentOnScrollForMyActivityRouter);
 app.use("/getContacts", getContactsRouter);
 app.use("/about", aboutUsRouter);
 
 app.listen(process.env.PORT || port, () => {
-    console.log(`the application started successfully on port ${port}`);
+  console.log(`the application started successfully on port ${port}`);
 });
--- static/script/f_list.js
+++ static/script/f_list.js
@@ -1,3 +1,28 @@
 $("#close").click(function () {
   $(".followersFollowing").css("display", "none");
 });
+$(".follow_button_flist").click(function () {
+  var user_id = $(this).attr("value");
+  console.log("F_list Button Clicked");
+  onClickFollowFList($(this)[0], user_id);
+});
+function onClickFollowFList(element, creatorId) {
+  var currentUserId = $(".profileDetails").attr("id");
+  if (creatorId !== currentUserId) {
+    $.ajax({
+      url: "/ajax/" + element.innerText,
+      type: "POST",
+      data: { creatorId: creatorId, postId: "123456789012" },
+      success: function (totalFollowers) {
+        showSnackbar(element.innerText + " Successfully", "success");
+        $("#f_btn" + creatorId).text() === "Follow" ? $("#f_btn" + creatorId).text("Unfollow") : $("#f_btn" + creatorId).text("Follow");
+      },
+      error: function (xhr, status, error) {
+        if (error === "Unauthorized") showSnackbar("You Are not LoggedIn!", "failure");
+        else showSnackbar("something Went Wrong!", "failure");
+      },
+    });
+  } else {
+    showSnackbar("You Can't Follow Your Self !", "failure");
+  }
+}
